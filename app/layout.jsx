d1fed0eb106a9e8d33143c9b72c69f4fda0b48e5
Oleh Medvid <meandjob@protonmail.com>
2023-05-24 23:51:58 +0300

    add: Nav component

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  title: "GoDeliver",
+  description: "Order delicious food & Share AI Prompts",
 };
 
 const RootLayout = ({ children }) => (
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
 // import Feed from "@components/Feed";
 
 const Home = () => (
-  <section className="w-full flex-center flex-col">
+  <section className="flex-center w-full flex-col">
     <h1 className="head_text text-center">
-      Hungry? Don`&apos;`t wait! Order delicious food for delivery in minutes.
+      Hungry? Don&apos;t wait! Order delicious food for delivery in minutes.
       <br className="max-md:hidden" />
-      <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      <span className="orange_gradient text-center"> GoDeliver</span>
     </h1>
     <p className="desc text-center">
       Discover GoDeliver, your go-to food delivery app. Order from a variety of
